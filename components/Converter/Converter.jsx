fbb5eb2b843764614199c2efa0e5efb4c38e200b
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2022-01-21 12:55:33 +0200

    converter in process

--- components/Converter/Converter.jsx
+++ components/Converter/Converter.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import styles from './Converter.module.css';
 
 function Converter() {
-  return <div>Converter</div>;
+  return (
+    <div className={styles.converterContainer}>
+      <div className={styles.bg}></div>
+      <form className={styles.form}>
+        <div className={styles.inputContainer}>
+          <input className={styles.input} type="number" placeholder="Pay" />
+          <input className={styles.input} type="number" placeholder="Buy" />
+        </div>
+        <p className={styles.text}>Payment method</p>
+        <select className={styles.select} name="payment" id="payment">
+          <option value="bank">Bank transfer</option>
+          <option value="debit">Debit card</option>
+          <option value="apple">Apple pay</option>
+        </select>
+        <button className={styles.btn}>Buy BTC</button>
+      </form>
+    </div>
+  );
 }
 
 export default Converter;
