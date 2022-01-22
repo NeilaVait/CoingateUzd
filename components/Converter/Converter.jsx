6e55242eca5e8ac787d5081bcf7fafacc1b6029b
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2022-01-22 14:51:50 +0200

    converter in process

--- components/Converter/Converter.jsx
+++ components/Converter/Converter.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './Converter.module.css';
+import btc from '../../public/arrowGreen.png';
+import Image from 'next/image';
 
 function Converter() {
   return (
@@ -7,8 +9,29 @@ function Converter() {
       <div className={styles.bg}></div>
       <form className={styles.form}>
         <div className={styles.inputContainer}>
-          <input className={styles.input} type="number" placeholder="Pay" />
-          <input className={styles.input} type="number" placeholder="Buy" />
+          <div className={styles.payContainer}>
+            <input className={styles.input} type="number" />
+            <span className={styles.pay}>Pay</span>
+
+            <select className={styles.payCurr} name="" id="">
+              <option id="eur" value="">
+                EUR
+              </option>
+              <option value="">EUR</option>
+              <option value="">EUR</option>
+              <option value="">EUR</option>
+            </select>
+          </div>
+          <div className={styles.buyContainer}>
+            <input className={styles.input} type="number" />
+            <span className={styles.buy}>Buy</span>
+            <select className={styles.buyCurr} name="" id="">
+              <option value="">BTC</option>
+              <option value="">BTC</option>
+              <option value="">BTC</option>
+              <option value="">BTC</option>
+            </select>
+          </div>
         </div>
         <p className={styles.text}>Payment method</p>
         <select className={styles.select} name="payment" id="payment">
