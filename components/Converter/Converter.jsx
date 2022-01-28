2c82bf0148dd9392fe78443bc46ac4353bce89fd
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2022-01-28 12:18:27 +0200

    updated converter calc

--- components/Converter/Converter.jsx
+++ components/Converter/Converter.jsx
@@ -4,15 +4,55 @@ import styles from './Converter.module.css';
 const ratesUrl = 'https://api.coingate.com/v2/rates';
 
 function Converter({ data }) {
-  const crypto = [...Object.keys(data.trader.buy)];
+  const cryptoArr = [...Object.keys(data.trader.buy)];
 
   const currAndCryptoArr = [...Object.keys(data.merchant.ADA)];
 
-  const currArr = currAndCryptoArr.filter((item) => !crypto.includes(item));
-  // console.log(currArr);
+  const crypto = currAndCryptoArr.filter((item) => cryptoArr.includes(item));
 
-  const [payCurrency, setPayCurrency] = useState('');
-  const [buyCurrency, setBuyCurrency] = useState('');
+  const currArr = currAndCryptoArr.filter((item) => !crypto.includes(item));
+
+  const [payCurrency, setPayCurrency] = useState('EUR');
+  const [buyCurrency, setBuyCurrency] = useState('BTC');
+
+  const [amountInPayCurrency, setAmountInPayCurrency] = useState('0');
+  const [amountInBuyCurrency, setAmountInBuyCurrency] = useState('0');
+
+  const calcBuyRate = (enteredValue) => {
+    // console.log(enteredValue);
+    setAmountInPayCurrency(enteredValue);
+    const rates = data.merchant[buyCurrency][payCurrency];
+    // console.log(rates);
+    const rate = 1 / rates;
+    const amount = enteredValue * rate;
+    // console.log(amount);
+    setAmountInBuyCurrency(amount.toFixed(7));
+  };
+
+  const calcPayRate = (enteredValue) => {
+    setAmountInBuyCurrency(enteredValue);
+    // console.log(enteredValue);
+    const rate = enteredValue * data.merchant[buyCurrency][payCurrency];
+    console.log(rate);
+    setAmountInPayCurrency(rate);
+  };
+
+  const setBuyCurrencyAndCalc = (currency) => {
+    console.log(currency);
+    if (amountInPayCurrency === 0) return;
+    setBuyCurrency(currency);
+    const rate = 1 / data.merchant[currency][payCurrency];
+    setAmountInBuyCurrency(rate.toFixed(7));
+  };
+
+  const setPayCurrencyAndCalc = (currency) => {
+    // console.log(currency);
+    if (amountInBuyCurrency === 0) return;
+    setPayCurrency(currency);
+
+    const rate = data.merchant[buyCurrency][currency];
+    setAmountInPayCurrency(rate);
+  };
 
   return (
     <>
@@ -20,9 +60,19 @@ function Converter({ data }) {
       <form className={styles.form}>
         <div className={styles.inputContainer}>
           <div className={styles.payContainer}>
-            <input className={styles.input} type="number" />
+            <input
+              maxLength={8}
+              className={styles.input}
+              type="number"
+              value={amountInPayCurrency}
+              onChange={(e) => calcBuyRate(e.target.value)}
+            />
             <span className={styles.pay}>Pay</span>
-            <select className={styles.payCurr} value={payCurrency} onChange={(e) => setPayCurrency(e.target.value)}>
+            <select
+              className={styles.payCurr}
+              value={payCurrency}
+              onChange={(e) => setPayCurrencyAndCalc(e.target.value)}
+            >
               {currArr.map((c) => (
                 <option key={c} value={c}>
                   {c}
@@ -31,9 +81,19 @@ function Converter({ data }) {
             </select>
           </div>
           <div className={styles.buyContainer}>
-            <input className={styles.input} type="number" />
+            <input
+              maxLength="8"
+              className={styles.input}
+              type="number"
+              value={amountInBuyCurrency}
+              onChange={(e) => calcPayRate(e.target.value)}
+            />
             <span className={styles.buy}>Buy</span>
-            <select className={styles.buyCurr} value={buyCurrency} onChange={(e) => setBuyCurrency(e.target.value)}>
+            <select
+              className={styles.buyCurr}
+              value={buyCurrency}
+              onChange={(e) => setBuyCurrencyAndCalc(e.target.value)}
+            >
               {crypto.map((c) => (
                 <option key={c} value={c}>
                   {c}
@@ -53,13 +113,5 @@ function Converter({ data }) {
     </>
   );
 }
-
-// export async function getServerSideProps(context) {
-//   const data = await fetch(ratesUrl);
-//   const rates = await data.json();
-//   console.log(rates);
-
-//   return { props: { currencies } };
-// }
 
 export default Converter;
